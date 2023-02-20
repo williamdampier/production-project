04ec53390dbdc71c5cea37c8dc7bef59c6266977
Vladimir Kuban <74879032+williamdampier@users.noreply.github.com>
2023-02-20 17:29:50 +1300

    Modal start

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,27 @@ import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/Navbar';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Sidebar } from 'widgets/SideBar';
+import { Modal } from 'shared/ui/Modal/ui/Modal';
+import { Button } from 'shared/ui/Button';
 import { AppRouter } from './providers/router';
 
 export default function App() {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const openModal = () => {
+        setIsOpen(true);
+    };
+
     const { theme } = useTheme();
     return (
         <div className={classNames('app', {}, [theme!])}>
             <Suspense fallback="">
                 <Navbar />
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+                {/* eslint-disable-next-line i18next/no-literal-string */}
+                <Button onClick={openModal}>toggle modal</Button>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
