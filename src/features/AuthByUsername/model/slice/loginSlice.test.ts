5e3895cdb841dff1b87bb7902f94c5fbf525d83a
Vladimir Kuban <74879032+williamdampier@users.noreply.github.com>
2023-03-01 16:03:07 +1300

    linting tests

--- src/features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -1,9 +1,9 @@
 import { DeepPartial } from '@reduxjs/toolkit';
-import { LoginSchema } from '../types/loginSchema';
+import { LoginSchema } from '../types/LoginSchema';
 import { loginActions, loginReducer } from './loginSlice';
 
 describe('loginSlice.test', () => {
-    test('test set username', () => {
+    test('set username', () => {
         const state: DeepPartial<LoginSchema> = { username: '123' };
         expect(loginReducer(
             state as LoginSchema,
@@ -11,7 +11,7 @@ describe('loginSlice.test', () => {
         )).toEqual({ username: '123123' });
     });
 
-    test('test set password', () => {
+    test('set password', () => {
         const state: DeepPartial<LoginSchema> = { password: '123' };
         expect(loginReducer(
             state as LoginSchema,
