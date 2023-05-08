9fa36baed7432c23c2ed0911e7634110d572c50c
Vladimir Kuban <74879032+williamdampier@users.noreply.github.com>
2023-05-08 19:48:50 +1200

    fixed fetchComments logic

--- src/entities/Comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Loader } from 'shared/ui/Loader/Loader';
 import { Avatar } from 'shared/ui/Avatar/Avatar';
 import { Text } from 'shared/ui/Text/Text';
 import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
--- src/pages/ArticleDetailPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -5,17 +5,36 @@ import { ArticleDetails } from 'entities/Article';
 import { useParams } from 'react-router-dom';
 import { Text } from 'shared/ui/Text/Text';
 import { CommentList } from 'entities/Comment';
+import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader';
+import { useSelector } from 'react-redux';
+import { getArticleCommentsError, getArticleCommentsIsLoading } from 'pages/ArticleDetailPage/model/selectors/comments';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { fetchCommentsByArticleId } from 'pages/ArticleDetailPage/model/services/fetchCommentsByArticleId/fetchCommentsByArticleId';
+import { articleDetailsCommentsReducer, getArticleComments } from '../../model/slices/articleDetailsCommentsSlice';
 import cls from './ArticleDetailPage.module.scss';
 
 interface ArticleDetailPageProps {
    className?: string;
 }
 
+const reducers:ReducersList = {
+    articleDetailsComments: articleDetailsCommentsReducer,
+};
+
 const ArticleDetailsPage: FC<ArticleDetailPageProps> = (props) => {
     const { className } = props;
     const { t } = useTranslation('article');
+    const dispatch = useAppDispatch();
     const { id } = useParams<{id:string}>();
+    const comments = useSelector(getArticleComments.selectAll);
 
+    const commentsIsLoading = useSelector(getArticleCommentsIsLoading);
+    const commentsError = useSelector(getArticleCommentsError);
+
+    useInitialEffect(() => {
+        dispatch(fetchCommentsByArticleId(id));
+    });
     if (!id) {
         return (
             <div className={classNames(cls.articleDetailPage, {}, [className])}>
@@ -24,15 +43,19 @@ const ArticleDetailsPage: FC<ArticleDetailPageProps> = (props) => {
         );
     }
     return (
-        // eslint-disable-next-line i18next/no-literal-string
-        <div className={classNames(cls.articleDetailPage, {}, [className])}>
-            <ArticleDetails id={id} />
-            <Text className={cls.title} title={t('Comments')} />
-            <CommentList
-                className={cls.title}
-                comments={[]}
-            />
-        </div>
+        <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+            <div className={classNames(cls.articleDetailPage, {}, [className])}>
+                <ArticleDetails id={id} />
+                <Text className={cls.title} title={t('Comments')} />
+                <CommentList
+                    isLoading={commentsIsLoading}
+                    className={cls.title}
+                    comments={comments}
+                />
+            </div>
+            {' '}
+
+        </DynamicModuleLoader>
     );
 };
 export default memo(ArticleDetailsPage);
